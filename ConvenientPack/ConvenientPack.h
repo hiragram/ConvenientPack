9f96fe3ff7b0f056b7c12c308bcf1938d405e69f
hiragram <y@yura.me>
2018-11-24 17:20:42 +0900

    Add Xcode project

---
+++ ConvenientPack/ConvenientPack.h
@@ -0,0 +1,19 @@
+//
+//  ConvenientPack.h
+//  ConvenientPack
+//
+//  Created by Yuya Hirayama on 2018/11/24.
+//  Copyright © 2018年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConvenientPack.
+FOUNDATION_EXPORT double ConvenientPackVersionNumber;
+
+//! Project version string for ConvenientPack.
+FOUNDATION_EXPORT const unsigned char ConvenientPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConvenientPack/PublicHeader.h>
+
+
